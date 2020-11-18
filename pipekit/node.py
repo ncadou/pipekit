43491d209fc0de2785d2bd6af5882c5e61a607fd
Nicolas Cadou <ncadou@intralinks.com>
2020-11-17 20:27:09 -0500

    move Message and Filter to own modules

--- pipekit/node.py
+++ pipekit/node.py
@@ -318,67 +318,8 @@ class Outbox(Manifold):
 
         self.debug('Finished sending')
         for pipe in self.channels.values():
-         try:
             self.debug(f'EOT to {pipe}')
             await pipe.send(Message.EOT)
-         except:
-            __import__('pudb').set_trace()
-
-
-class Filter(Component):
-
-    def __call__(self, messages):
-        return self.filter(messages)
-
-    async def filter(self, messages):
-        async for channel, message in messages:
-            channel, message = await self.process(channel, message)
-            yield (channel, message)
-
-            if not self.running:
-                break
-
-    async def process(self, channel, message):
-        raise NotImplementedError(f'process() out {self}')
-
-
-class ChannelChain(Filter):
-
-    TESTS = dict(directory=lambda m: m.path.id_dir(),
-                 file=lambda m: m.path.id_file())
-
-    async def process(self, channel, message):
-        for test_name, test_fn in self.TESTS.items():
-            if test_fn(message):
-                return test_name, message
-
-
-class MessageMangleFilter(Filter):
-
-    def deepget(self, mapping, key):
-        if '.' not in key:
-            return mapping[key]
-
-        else:
-            current, remainder = key.split('.', 1)
-            return self.deepget(mapping[current], remainder)
-
-    def deepset(self, mapping, key, value):
-        if '.' not in key:
-            mapping[key] = value
-
-        else:
-            current, remainder = key.split('.', 1)
-            self.deepset(mapping[current], remainder, value)
-
-    async def process(self, channel, message):
-        for spec in self.settings.attributes:
-            if len(spec) != 1:
-                raise ValueError(f'Bad configuration: {self.settings}')
-
-            key = list(spec.keys())[0]
-            self.deepset(message, key, spec[key].format(**message))
-        return channel, message
 
 
 class ThreadedNode(Node):
