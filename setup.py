f03abbeb2c17d5f95fcabd4dd1a44a946cb5d9b2
Nicolas Cadou <ncadou@intralinks.com>
2021-06-18 22:53:20 -0400

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
 
 setup(
     name='pipekit',
-    version='0.1.11',
+    version='0.2.0',
     description='Tools for flow-based programming',
     long_description=long_description,
     url='https://github.com/ncadou/pipekit',
